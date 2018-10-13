63b4fb6b8e2ceffeeb096e70a32b6fb533c61e0f
Alexandre <franciscoalexandre.m.c@gmail.com>
2018-10-13 16:14:58 -0300

    recomitando busca em profundidade reecursiva

--- IA/src/AlgoritmosDeBusca.java
+++ IA/src/AlgoritmosDeBusca.java
@@ -56,16 +56,32 @@ public class AlgoritmosDeBusca {
         return null;
     }
     
-    public Regua buscaProfundidade(Regua regua, Regua estadoMeta) {
-    	//lista de estados
-        listaDeEstados = new ArrayList<Regua>();
-         //adicionando o estado inicial
-         listaDeEstados.add(regua);
-         
-         
-         while(!listaDeEstados.isEmpty()) {
-        	 
-         }
+    /**
+     * Algoritmo de busca em profundidade recursiva
+     * @param regua Regua do estado inicial
+     * @param estadoMeta Regua do estado méta. o algoritmo para quando esse estado é alcançado
+     * @return o caminho do estado inicial até o estado meta ou NULL caso não exista uma solução
+     */
+    private Regua DepthFirstSearch(Regua regua, Regua estadoMeta) {
+        //CASO BASE -- se o estado for meta retorna o estado
+        if(regua.equals(estadoMeta))
+            return regua;
+        //lista de estados
+        Queue<Regua> listaDeEstados = new LinkedList<>();
+        //adicionando o estado inicial
+        listaDeEstados.add(regua);
+        //marcando o estado inicial como visitado
+        regua.visitado = true;
+        //buscando os sucessores da regua
+        Iterator<Regua> reguas = listaDeEstados.remove().sucessores().iterator();
+        //enquanto o estado possuir sucessores
+        while (reguas.hasNext()) {
+            Regua r = reguas.next();
+            //visita o proximo estado sucessor se este ainda não foi visitado
+            if (!r.visitado)
+                DepthFirstSearch(r, estadoMeta);
+        }
+        return null;
     }
     
 }
