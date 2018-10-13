f84fd77779e20e68fc728c5bcf0b412510169490
george <george@george-S14BW01>
2018-10-13 13:01:22 -0300

    Inicio da correção

--- IA/src/AlgoritmosDeBusca.java
+++ IA/src/AlgoritmosDeBusca.java
@@ -7,18 +7,7 @@ import java.util.Iterator;
  */
 public class AlgoritmosDeBusca {
      
-    private void DepthFirstSearch(int i, boolean[] visitados, Grafo espacoDeBusca) {
-        //sinalizando que o vertice i foi visitado
-        visitados[i] = true;
-        //carregando lista de vertices adjacentes a i
-        Iterator<Integer> listaDeAdjacentes = espacoDeBusca.arestas[i].iterator();
-        //enquanto houver vertices adjacentes ao vertice i
-        while (listaDeAdjacentes.hasNext()) {
-            //pega o proximo vertice da lista de adjacencia
-            int n = listaDeAdjacentes.next();
-            //e visita este vertice caso ainda não tenha sido visitado
-            if (!visitados[n])
-                DepthFirstSearch(n, visitados, espacoDeBusca);
-        }
+    private void DepthFirstSearch() {
+        
     }
 }
