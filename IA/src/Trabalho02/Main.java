6fa25548ab9771fd7902b7cff48f3a7f7859aace
george <george@george-S14BW01>
2018-11-20 17:39:45 -0300

    De novo

--- IA/src/Trabalho02/Main.java
+++ IA/src/Trabalho02/Main.java
@@ -4,8 +4,8 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Rainhas r=new Rainhas(8);
-		r=Rainhas.rainhaAleatoria(8);
+		Rainhas r=new Rainhas(15);
+		r=Rainhas.rainhaAleatoria(15);
 		Buscas b= new Buscas();
 		r=b.subidaE(r);
 		r.imprime();
